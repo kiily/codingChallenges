b1b4d773c94b02e4f083bb75c20cd10f1755c429
miguelyoobic95 <mmarin@yoobic.com>
2019-07-29 20:02:39 +0100

    feat(camelcase): add camelcase solution

---
+++ camelcase.js
@@ -0,0 +1,19 @@
+const LOWERCASE_LOWER = 'a'.charCodeAt(0);
+const LOWERCASE_UPPER = 'z'.charCodeAt(0);
+const UPPERCASE_LOWER = 'A'.charCodeAt(0);
+const UPPERCASE_UPPER = 'Z'.charCodeAt(0);
+
+export function camelCase(str) {
+    if (str === '') {
+        return 0;
+    };
+    // Total words start at one; then forEach capital letter we add a word
+    let totalWords = 1;
+    for (let i = 0; i < str.length; i++) {
+        const charCode =  str.charCodeAt(i);
+        if (charCode >= UPPERCASE_LOWER && charCode <= UPPERCASE_UPPER) {
+            totalWords++;
+        }
+    }
+    return totalWords;
+}
---
+++ camelcase.spec.js
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/camelcase/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+import { camelCase } from './camelcase';
+
+describe('caesacamelCaserCypher', () => {
+    it.each`
+        string | expected 
+        ${'abc'} | ${1}
+        ${'aLongWord'} | ${3} 
+        ${`camelCase`} | ${2}
+        ${'aVeryLongCamelCaseIndeed'} | ${6}
+        ${''} | ${0}
+        ${'zYIM'} | ${4}
+        ${'aVeryLongCamelCaseIndeedButLongerThanEverBefore'} | ${11}
+        ${'aVeryLongCamelCaseIndeedButLongerThanEverBeforeToToToTo'} | ${15}
+        ${'aVeryLongCamelCaseIndeedButLongerThanEverBeforeToToToToABCD'} | ${19}
+        `('outputs number of words in camelcase string', ({ string, expected}) => {
+        expect(camelCase(string)).toBe(expected);
+    })
+});
